af492fe27de708866a2e422bc78dbdb19eb21d00
Sander in 't Veld <sanderintveld@hotmail.com>
2021-04-18 20:32:22 +0200

    Send version and login information to server.

--- src/epicinium_client.py
+++ src/epicinium_client.py
@@ -5,6 +5,8 @@ import logging
 import discord
 from discord.ext import commands, tasks
 import aiohttp
+import asyncio
+import struct
 
 log = logging.getLogger(__name__)
 
@@ -17,22 +19,20 @@ class EpiciniumClient(commands.Cog):
 		self.account_id = config['epicinium-account-id']
 		self.session_token = config['epicinium-session-token']
 		user_agent = "epicinium-bot/{} (python)".format(self.version)
-		log.debug("User-Agent: " + user_agent)
 		self.session = aiohttp.ClientSession(
 		    headers={"User-Agent": user_agent}, raise_for_status=True)
 		self.listen.start()
-		log.info("Client started.")
+		log.info("Client started: {}".format(user_agent))
 
 	def cog_unload(self):
 		log.info("Client stopped.")
 
 	@tasks.loop(seconds=10)
 	async def listen(self):
-		log.debug("Tick.")
 		host, port = await self.get_server()
 		log.info("Connecting to {}:{}...".format(host, port))
-		# todo
-		log.debug("Tock.")
+		await self.connect(host=host, port=port)
+		log.debug("Connection ended.")
 
 	@listen.after_loop
 	async def on_listen_cancel(self):
@@ -47,3 +47,32 @@ class EpiciniumClient(commands.Cog):
 		host = body['host']
 		port = body['port']
 		return host, port
+
+	async def connect(self, *, host, port):
+		reader, writer = await asyncio.open_connection(host, port)
+		message = {
+		    'type': 'version',
+		    'version': self.version,
+		    'metadata': {
+		        'patchmode': 'bot'
+		    }
+		}
+		writer.write(encode_message(message))
+		message = {
+		    'type': 'join_server',
+		    'sender': self.account_id,
+		    'content': self.session_token
+		}
+		writer.write(encode_message(message))
+		await writer.drain()
+		# todo
+		writer.close()
+
+
+def encode_message(message):
+	# Encode the json message in lower ASCII.
+	data = json.dumps(message, ensure_ascii=True).encode(encoding='ascii')
+	# Prepend the network order representation of the 32-bit length.
+	head = struct.pack('!I', len(data))
+	log.debug("Sending message of length {}".format(len(data)))
+	return head + data
